e6507e6dc23d874028ed77fb5bb64765197f6aa9
RaghulR238 <123478701+RaghulR238@users.noreply.github.com>
2024-02-21 21:55:16 +0530

    Update up.js

--- src/upload.js/up.js
+++ src/upload.js/up.js
@@ -124,7 +124,7 @@ export default function Up() {
     const url=await uploads(file);
     console.log("url is  :  ",url);
     try{
-      await axios.post("http://localhost:3002/api/upload/up",{
+      await axios.post("https://main--graceful-kleicha-2d0c35.netlify.app/api/upload/up",{
         ...upload,
         image:url
       },{
