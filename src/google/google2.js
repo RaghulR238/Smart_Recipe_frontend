391151044a2bf3a4e01498a2a5d9693ad1435321
RaghulR238 <123478701+RaghulR238@users.noreply.github.com>
2024-02-22 09:09:46 +0530

    Update google2.js

--- src/google/google2.js
+++ src/google/google2.js
@@ -131,7 +131,7 @@ export default function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "https://main--graceful-kleicha-2d0c35.netlify.app/auth/login",
+        "https://smart-recipe-backend.onrender.com/api/auth/login",
         //{ username, password },
         {...upload},
         { withCredentials: true }
